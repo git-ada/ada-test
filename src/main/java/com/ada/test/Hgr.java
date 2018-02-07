1935f48b0a5bfb49069905f7c96c4545bc87e922
hgr <ASUS@DESKTOP-8SAE4VC>
2018-02-07 15:05:28 +0800

    合并5.0

--- src/main/java/com/ada/test/Hgr.java
+++ src/main/java/com/ada/test/Hgr.java
@@ -5,11 +5,16 @@ public class Hgr {
 	public static void main(String[] args) {
 
 
+
 		  System.out.println("这是master版本");
 		
 
 
 		System.out.println("这是分支版本");
+
+
+		System.out.println("这是分支版本");
+		System.out.println("新建");
 
 	}
 }
