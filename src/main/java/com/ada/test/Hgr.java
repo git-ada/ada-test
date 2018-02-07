54f0e23a5ce121b8e63b8f5a58a8fc2bc3361077
hgr <ASUS@DESKTOP-8SAE4VC>
2018-02-07 14:49:27 +0800

    xx

--- src/main/java/com/ada/test/Hgr.java
+++ src/main/java/com/ada/test/Hgr.java
@@ -4,9 +4,7 @@ public class Hgr {
 
 	public static void main(String[] args) {
 
-		System.out.println("这是master版本");
-
-		System.out.println("这是hgr分支版本");
-
+		  System.out.println("这是master版本");
+		
 	}
 }
